ae67d4fce485fb2016069acde8bd8aac76adfbab
Marcus Hammarberg <marcusoft.net@gmail.com>
2021-04-19 10:17:33 +0200

    Adds a dependency update part of saltVerify.sh

--- saltVerify.sh
+++ saltVerify.sh
@@ -4,6 +4,15 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPTPATH
 
+AUDITFIXDONE=false
+AUDITFIXMESSAGE=''
+auditresult=$(npm audit --silent)
+if [[ "$auditresult" == *"address all issues"* ]]
+then
+  auditresult=$(npm audit fix --force --silent)
+  AUDITFIXDONE=true
+fi
+
 installresult=$(npm i --silent)
 if [[ "$installresult" == *"high"* ]]
 then
@@ -21,8 +30,16 @@ then
 fi
 
 
+if [ "$AUDITFIXDONE" = true ] ; then
+    echo "Dependencies audit done, fixed and the script still works - committing changes"
+    git add package*
+    git commit -m "Audit fixed by script"
+    git push
+    AUDITFIXMESSAGE='(Dependecies updated)'
+fi
+
 # cd back to where we came from
 cd - > /dev/null
 
-echo "Installation, linting and testing verified - OK"
+echo "Installation, linting and testing verified $AUDITFIXMESSAGE - OK"
 exit 0
