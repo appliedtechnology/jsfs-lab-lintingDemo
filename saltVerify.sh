4160a15fcbf5cef95a1bc4d6307b0be14f899914
Marcus Hammarberg <marcusoft.net@gmail.com>
2022-04-29 11:43:12 +0200

    Removes audit fix in saltVerify.sh

--- saltVerify.sh
+++ saltVerify.sh
@@ -4,14 +4,7 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPTPATH
 
-AUDITFIXDONE=false
-AUDITFIXMESSAGE=''
-auditresult=$(npm audit --silent)
-if [[ "$auditresult" == *"address all issues"* ]]
-then
-  auditresult=$(npm audit fix --force --silent)
-  AUDITFIXDONE=true
-fi
+
 
 installresult=$(npm i --silent)
 if [[ "$installresult" == *"high"* ]]
@@ -45,16 +38,10 @@ then
 fi
 
 
-if [ "$AUDITFIXDONE" = true ] ; then
-    echo "Dependencies audit done, fixed and the script still works - committing changes"
-    git add package*
-    git commit -m "Audit fixed by script" -n
-    git push
-    AUDITFIXMESSAGE='(Dependecies updated)'
-fi
+
 
 # cd back to where we came from
 cd - > /dev/null
 
-echo "Installation, linting$LINTFIXMESSAGE and testing verified $AUDITFIXMESSAGE - OK"
+echo "Installation, linting$LINTFIXMESSAGE and testing verified - OK"
 exit 0
